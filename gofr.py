a114e77b906966d88acd957cf8e9a4a7fdf27a91
sally008 <sarah.allec@gmail.com>
2020-08-27 14:06:25 -0700

    Final commit for little g

--- gofr.py
+++ gofr.py
@@ -17,6 +17,7 @@ class GofR():
         self.spec_indices = {}
         for s in self.species:
             self.spec_indices[s] = [i for i,v in enumerate(self.elements) if v == s]
+        self.N = 0
 
     def volume( self, LCC ):
         ''' Computes the volume of the simulation box
@@ -43,7 +44,7 @@ class GofR():
     
         return N/V
 
-    def r_ij( self, LCC, positions ):
+    def r_ij( self, LCC, positions, indices1, indices2 ):
         ''' Computes r_ij for a given timestep 
     
         args:
@@ -55,10 +56,14 @@ class GofR():
                  periodic boundaries) of all pairs of atoms'''
     
         r = positions
-        n_atoms = len(positions)        # number of atoms
+
+        if indices1 != None and indices2 != None:
+            v_ij = [ np.abs(np.subtract(r[j], r[i])) for i in indices1 for j in indices2 if j != i ]
+        else:
+            n_atoms = len(positions)        # number of atoms
+            v_ij = [ np.abs(np.subtract(r[j], r[i])) for i in range(n_atoms) for j in range(i+1, n_atoms) ]
+
         LCC_inv = np.linalg.inv(LCC)    # inverse of lattice vec matrix
-        v_ij = [ np.abs(np.subtract(r[j], r[i])) for i in range(n_atoms) for j in range(i+1, n_atoms) ]
-        
         vp_ij = [ np.mod( ( np.dot(LCC_inv,v) + 0.5), 1 ) - 0.5 for v in v_ij ] 
         vp_ij = [ np.matmul( LCC, vp) for vp in vp_ij ] 
     
@@ -83,7 +88,7 @@ class GofR():
         b = - 1 / (sigma * sigma)
         return [a[i]*np.exp(b*(r_grid-r)**2) for i,r in enumerate(r_ij)]
 
-    def rho( self, cell, positions, ngrid, sigma ):
+    def rho( self, cell, positions, ngrid, sigma, indices1=None, indices2=None ):
         ''' Computes rho, the radial density, as a function of r.
     
         args:
@@ -96,12 +101,16 @@ class GofR():
         returns:
             rho = (array) the radial density as a function of r'''
         
-        N = len(positions) # need to change this if reading in an entire trajectory
-        curr_r_ij = self.r_ij(cell,positions) 
-        r_grid = np.linspace(0.5,ceil(max(curr_r_ij)),ngrid)
-        return [r_grid, np.divide( 2*np.sum( self.f(r_grid, curr_r_ij, sigma), axis = 0 ), N)]
+        curr_r_ij = self.r_ij(cell,positions,indices1,indices2) 
+        r_grid = np.linspace(0.5,8.0,ngrid)
+        if indices1 == None or indices2 == None:
+            return [r_grid, np.divide( 2*np.sum( self.f(r_grid, curr_r_ij, sigma), axis = 0 ), self.N )]
+        else:
+            N1 = len(indices1)
+            N2 = len(indices2)
+            return [r_grid, np.divide( self.N*np.sum( self.f(r_grid, curr_r_ij, sigma), axis = 0 ), (N1*N2) )]
 
-    def little_g( self, step, sigma=0.15, ngrid = 100 ):
+    def little_g( self, step, sigma=0.15, ngrid = 100, el1=None, el2=None ):
         ''' Computes R(r), the radial distribution function, and 
         g(r), the pair distribution function.
     
@@ -113,15 +122,23 @@ class GofR():
             r = (array) the grid over which the Gaussian is compute
             g = (array) the pair distribution function for all element types
             R = (array) the radial distribution function for all element types'''
-   
-           
+
         positions = self.traj[step].get_positions()
         cell = self.traj[step].get_cell()
-    
-        N = len(positions)
+        self.N = len( positions ) 
+   
         V = self.volume(cell)
-    
-        rho_r = self.rho(cell,positions,ngrid,sigma)
-        r = rho_r[0]
-        g = rho_r[1]/self.rho_0(N,V)
+       
+        if el1 != None and el2 != None:
+            indices1 = self.spec_indices[el1]
+            indices2 = self.spec_indices[el2]
+            pair = el1+"-"+el2
+            coeff = self.N / (len(indices1)*len(indices2))
+            rho_r = self.rho(cell,positions,ngrid,sigma,indices1,indices2)
+
+        else:
+            coeff = self.N
+            rho_r = self.rho(cell,positions,ngrid,sigma)
+        r = rho_r[0] 
+        g = rho_r[1]/self.rho_0(self.N,V)
         return np.transpose([r, g])
