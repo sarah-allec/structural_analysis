7a3a0dde00b5e6cc2a250e008cf62696313010de
sally008 <sarah.allec@gmail.com>
2020-10-16 13:20:59 -0700

    Add sample plotting script

--- plot.py
+++ plot.py
@@ -1,19 +1,14 @@
-import numpy as np
-import sys
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
 from matplotlib import axes
 
-filename=sys.argv[1] # make default
-file=open(filename)
-data=[float(i) for i in file.readlines()]
-file.close()
-
-label=sys.argv[2] # make default
-
 #plots
 params = {
+    'font.family': 'serif',
+    'font.serif': 'Times',
     'axes.labelsize': 14,
+    'axes.xmargin': 0,
+    'axes.ymargin': 0,
     'legend.fontsize': 12,
     'xtick.labelsize': 10,
     'ytick.labelsize': 10,
@@ -22,20 +17,13 @@ params = {
     }
 rcParams.update(params)
 
-# get units
-if label == "Energy":
-    units = "eV"
-if label == "Pressure":
-    units = "kb"
-if label == "Temperature":
-    units = "K"
-
-#plt.style.use('dark_background')
 fig,ax=plt.subplots()
-ax.plot(range(len(data)),data)
-ax.set_xlabel("Timestep")
-ax.set_ylabel(label + " (" + units+")")
+ax.plot(r_crystal,g_crystal,"-",color="black",label=r'$c-\mathrm{In}_{2}\mathrm{O}_{3}$')
+ax.plot(r_amorphous,g_amorphous,"-",color="dodgerblue",label=r'$a-\mathrm{In}_{2}\mathrm{O}_{3}$')
+
+ax.set_xlabel(r'r ($\mathrm{\AA}$)')
+ax.set_ylabel("g(r)")
 ax.legend(loc="best")
 fig.tight_layout()
-fig.savefig(label+".png",dpi=600)
+fig.savefig("gofr_in2o3.png",dpi=600)
 
